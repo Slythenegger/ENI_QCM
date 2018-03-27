7466aadffeb8c74ab1f032e3c12b2a7fb72e935f
Unknown <slythenegger@hotmail.fr>
2018-03-27 11:03:15 +0200

    Bo User

---
+++ src/fr/eni/qcm/BO/User.java
@@ -0,0 +1,97 @@
+package fr.eni.qcm.BO;
+
+public class User {
+	
+	private int idUser, idPromo;
+	private String login, password, nom, prenom, email, role;
+	
+	
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+
+	public int getIdPromo() {
+		return idPromo;
+	}
+
+
+	public void setIdPromo(int idPromo) {
+		this.idPromo = idPromo;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+
+}
